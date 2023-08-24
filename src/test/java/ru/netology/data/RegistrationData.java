8d622e246b68175c71f49812105a41935048eb8c
Marija Holzeimer <diatmin@gmail.com>
2023-08-24 20:02:39 +0300

    Add annotation Value

--- src/test/java/ru/netology/data/RegistrationData.java
+++ src/test/java/ru/netology/data/RegistrationData.java
@@ -2,9 +2,11 @@ package ru.netology.data;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 
 @Data
 @RequiredArgsConstructor
+@Value
 public class RegistrationData {
     private final String city;
     private final String name;
