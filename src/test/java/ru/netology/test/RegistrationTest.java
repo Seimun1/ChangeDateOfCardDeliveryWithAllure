972826bf9cef7f597633ae235b696d50c17fb165
Marija Holzeimer <diatmin@gmail.com>
2023-08-25 12:59:55 +0300

    Comb test

--- src/test/java/ru/netology/test/RegistrationTest.java
+++ src/test/java/ru/netology/test/RegistrationTest.java
@@ -17,24 +17,25 @@ public class RegistrationTest {
         open("http://localhost:9999/");
 
         RegistrationData userData = DataGenerator.generateUser("ru");
-
+        int addDays1 = 5;
+        int addDays2 = 7;
         $("[data-test-id='city'] input").sendKeys(userData.getCity());
-        $("[data-test-id='date'] input").sendKeys(generateDate(5));
-        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(5));
+        $("[data-test-id='date'] input").sendKeys(generateDate(addDays1));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(addDays1));
         $("[data-test-id='name'] input").sendKeys(userData.getName());
         $("[data-test-id='phone'] input").sendKeys(userData.getPhone());
         $("[data-test-id='agreement']").click();
         $x("//span[text()='Запланировать']").click();
         $x("//div[@data-test-id='success-notification']")
-                .shouldBe(visible, Duration.ofSeconds(4))
-                .shouldHave(exactText("Успешно! Встреча успешно запланирована на " + DataGenerator.generateDate(5)));
+                .shouldBe(visible)
+                .shouldHave(exactText("Успешно! Встреча успешно запланирована на " + DataGenerator.generateDate(addDays1)));
 
-        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(addDays2));
         $x("//span[text()='Запланировать']").click();
         $x("//span[text()='Перепланировать']").click();
         $x("//div[@data-test-id='success-notification']")
-                .shouldBe(visible, Duration.ofSeconds(4))
-                .shouldHave(exactText("Успешно! Встреча успешно запланирована на " + generateDate(7)));
+                .shouldBe(visible)
+                .shouldHave(exactText("Успешно! Встреча успешно запланирована на " + generateDate(addDays2)));
     }
 
 }
